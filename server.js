45fffcff21f18c559148329a12cfb1997365cecf
DELL <rajkumartamang182@gmail.com>
2022-09-22 20:44:44 +0545

    updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import { readdirSync } from "fs";
+import compression from "compression";
 
 const app = express();
 const morgan = require("morgan");
@@ -21,6 +22,18 @@ mongoose
   });
 
 // Middleware
+app.use(
+  compression({
+    level: 6,
+    threshold: 0,
+    filter: (req, res) => {
+      if (req.headers["x-no-compression"]) {
+        return false;
+      }
+      return compression.filter(req, res);
+    },
+  })
+);
 app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: "true" }));
